f82a8b056f6f1d7eba942abf453ca49c6f8ce6d6
Androcode <git@androcode.com>
2015-07-27 15:27:36 +0200

    Travis #

--- build.sh
+++ build.sh
@@ -12,20 +12,20 @@ set -e
 # build site with jekyll, by default to `_site' folder
 jekyll build
 
-# cleanup and copy generated HTML site to `master' branch
+# cleanup
 rm -rf ../androcode-es.github.io.master
-mkdir ../androcode-es.github.io.master
-cp -R _site/* ../androcode-es.github.io.master
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-cd ../androcode-es.github.io.master
-git init
-git remote add origin https://${GH_TOKEN}@github.com/androcode-es/androcode-es.github.io.git
+git clone -b master https://${GH_TOKEN}@github.com/androcode-es/androcode-es.github.io.git ../androcode-es.github.io.master
+
+# copy generated HTML site to `master' branch
+cp -R _site/* ../androcode-es.github.io.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
-git config user.email "git@androcode.es"
+cd ../androcode-es.github.io.master
+git config user.email "git@androcode.com"
 git config user.name "Androcode"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet origin master > /dev/null 2>&1 
+git push --quiet origin master -u
