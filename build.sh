ebc2961ae211626aaa4b93e793b2928b654db4fb
Sloy <rafa.vazsan@gmail.com>
2015-07-27 14:08:59 +0200

    Build y publicación del sitio mediante Travis
    Se configura Travis para que compile el blog y lo publique en la rama master del repositorio, evitando que sea GitHub quien compile el blog.
    Esto se hace para poder activar plugins de Jekyll que GitHub no soporta.
    Tiene la desventaja de que hay que mantener 2 ramas separadas: master con el contenido generado, y develop (o jekyll, está por decidir) donde publicar los cambios reales del código.
    
    El procedimiento está sacado mayormente de http://eshepelyuk.github.io/2014/10/28/automate-github-pages-travisci.html

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# only proceed script when started not by pull request (PR)
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# build site with jekyll, by default to `_site' folder
+jekyll build
+
+# cleanup
+rm -rf ../androcode-es.github.io.master
+
+#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
+git clone https://${GH_TOKEN}@github.com/androcode-es/androcode-es.github.io.git ../androcode-es.github.io.master
+
+# copy generated HTML site to `master' branch
+cp -R _site/* ../androcode-es.github.io.master
+
+# commit and push generated content to `master' branch
+# since repository was cloned in write mode with token auth - we can push there
+cd ../androcode-es.github.io.master
+git config user.email "git@androcode.com"
+git config user.name "Androcode"
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin master > /dev/null 2>&1 
